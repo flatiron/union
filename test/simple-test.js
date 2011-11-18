bcd35d1afd893a8c6fd8d8d79e1559d1026c0290
indexzero <charlie.robbins@gmail.com>
2011-11-18 00:11:44 -0500

    [test] Add first basic unit tests for union against `examples/simple.js`

---
+++ test/simple-test.js
@@ -0,0 +1,45 @@
+/*
+ * simple-test.js: Simple tests for basic streaming and non-streaming HTTP requests with union.
+ *
+ * (C) 2011, Nodejitsu Inc.
+ * MIT LICENSE
+ *
+ */
+
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    spawn = require('child_process').spawn,
+    request = require('request'),
+    vows = require('vows');
+
+var examplesDir = path.join(__dirname, '..', 'examples'),
+    simpleScript = path.join(examplesDir, 'simple.js'),
+    server;
+
+vows.describe('union/simple').addBatch({
+  "When using union": {
+    "a simple http server": {
+      topic: function () {
+        server = spawn('node', [simpleScript]);
+        server.stdout.on('data', this.callback.bind(this, null));
+      },
+      "a GET request to `/foo`": {
+        topic: function () {
+          request({ uri: 'http://localhost:8080/foo' }, this.callback);
+        },
+        "it should respond with `hello world`": function (err, res, body) {
+          assert.isTrue(!err);
+          assert.equal(res.statusCode, 200);
+          assert.equal(body, 'hello world\n');
+        }
+      }
+    }
+  }
+}).addBatch({
+  "When the tests are over": {
+    "the server should close": function () {
+      server.kill();
+    }
+  }
+}).export(module);
