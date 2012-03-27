148da2f51e94deb90dd851663059fbb8bf4e2fd1
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2012-03-27 16:46:06 +0530

    Merge pull request #15 from flatiron/gh-12

--- test/streaming-test.js
+++ test/streaming-test.js
@@ -1,6 +1,8 @@
 var assert = require('assert'),
-    vows = require('vows'),
+    fs = require('fs'),
+    path = require('path'),
     request = require('request'),
+    vows = require('vows'),
     union = require('../');
 
 vows.describe('union/streaming').addBatch({
@@ -24,6 +26,35 @@ vows.describe('union/streaming').addBatch({
       },
       'should receive complete POST data': function (chunks) {
         assert.equal(chunks, 'hello world');
+      }
+    },
+    "a simple pipe to a file": {
+      topic: function () {
+        var self = this;
+
+        union.createServer({
+          before: [
+            function (req, res, next) {
+              var filename = path.join(__dirname, 'fixtures', 'pipe-write-test.txt'),
+                  writeStream = fs.createWriteStream(filename);
+
+              req.pipe(writeStream);
+              writeStream.on('close', function () {
+                res.writeHead(200);
+                fs.createReadStream(filename).pipe(res);
+              });
+            }
+          ]
+        }).listen(9044, function () {
+          request({
+            method: 'POST',
+            uri: 'http://localhost:9044',
+            body: 'hello world'
+          }, self.callback);
+        });
+      },
+      'should receive complete POST data': function (err, res, body) {
+        assert.equal(body, 'hello world');
       }
     }
   }
